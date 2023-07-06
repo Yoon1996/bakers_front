34933726f226f52439b2b199745f44986b23c56d
Seongjun Yoon <dhkdclxk1996@gmail.com>
2023-07-06 14:31:32 +0900

    project_init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Calendar, DatePicker, Space, version } from "antd";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <DatePicker />
+        <Button danger={true}>Button</Button>
+        <div className='calendar_wrap'>
+        <Calendar></Calendar>
+        </div>
     </div>
   );
 }
